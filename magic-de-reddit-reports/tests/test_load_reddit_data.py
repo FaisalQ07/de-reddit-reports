36950874ec960e1abe4ef9904fc3a36c3860be3e
FaisalQ07 <qureshi.faisal712@gmail.com>
2024-03-18 21:26:01 +0000

    tested test module with first test function

--- magic-de-reddit-reports/tests/test_load_reddit_data.py
+++ magic-de-reddit-reports/tests/test_load_reddit_data.py
@@ -3,9 +3,11 @@ from unittest.mock import patch, MagicMock
 import pandas as pd
 from datetime import datetime
 
-from magic-de-reddit-reports.data_loaders.load_reddit_data import convert_to_local_time
+import sys
+sys.path.append('/home/src/magic-de-reddit-reports')
+from data_loaders.load_reddit_data import load_data_from_api, convert_to_local_time
 
-class TestLoadRedditData(unittest.Testcase):
+class TestLoadRedditData(unittest.TestCase):
     def test_convert_to_local_time(self):
         utc_datetime = datetime(2024, 3, 18, 12, 0, 0)
         # Call the function
@@ -18,3 +20,4 @@ class TestLoadRedditData(unittest.Testcase):
         self.assertEqual(local_datetime.month, 3)
         self.assertEqual(local_datetime.day, 18)
         self.assertEqual(local_datetime.hour, 8)  # Adjusted to Toronto time
+        
