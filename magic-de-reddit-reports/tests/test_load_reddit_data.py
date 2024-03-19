0a6d77b5f7aff6091f2178d33bb92a07f1c426b3
FaisalQ07 <qureshi.faisal712@gmail.com>
2024-03-19 19:39:46 +0000

    added test test_read_metadata. Tested successfully

--- magic-de-reddit-reports/tests/test_load_reddit_data.py
+++ magic-de-reddit-reports/tests/test_load_reddit_data.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 import sys
 sys.path.append('/home/src/magic-de-reddit-reports')
-from data_loaders.load_reddit_data import load_data_from_api, convert_to_local_time
+from data_loaders.load_reddit_data import load_data_from_api, convert_to_local_time, read_metadata
 
 class TestLoadRedditData(unittest.TestCase):
     def test_convert_to_local_time(self):
@@ -20,4 +20,23 @@ class TestLoadRedditData(unittest.TestCase):
         self.assertEqual(local_datetime.month, 3)
         self.assertEqual(local_datetime.day, 18)
         self.assertEqual(local_datetime.hour, 8)  # Adjusted to Toronto time
+
+    @patch('data_loaders.load_reddit_data.storage.Client')
+    @patch('data_loaders.load_reddit_data.pd.read_parquet')
+    def test_read_metadata(self, mock_read_parquet, mock_client):
+        # Mocking client, bucket, and blob
+        mock_bucket = MagicMock()
+        mock_blob = MagicMock()
+        mock_bucket.blob.return_value = mock_blob
+        mock_client.return_value.get_bucket.return_value = mock_bucket
+
+        # Mocking DataFrame returned by pd.read_parquet
+        expected_df = MagicMock()
+        mock_read_parquet.return_value = expected_df
+
+        # Call the function
+        metadata_df = read_metadata(bucket_name='test_bucket', sub_reddit='test_subreddit')
+
+        # Assertions
+        self.assertEqual(metadata_df, expected_df)
         
